3d9d91b828505632e7543191700d292ebc6efa4c
Creighton Medley <creighton.medley@lexmark.com>
2022-12-31 16:16:26 -0500

    initial commit

---
+++ after/plugin/vimtest.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>t", vim.cmd.TestNearest, { silent = true });
+vim.keymap.set("n", "<leader>T", vim.cmd.TestFile, { silent = true });
+vim.keymap.set("n", "<leader>A", vim.cmd.TestSuite, { silent = true });
---
+++ lua/cmedley/init.lua
@@ -0,0 +1,8 @@
+require('cmedley.set')
+require('cmedley.remap')
+require('cmedley.packer')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.mix_format_on_save = 1
